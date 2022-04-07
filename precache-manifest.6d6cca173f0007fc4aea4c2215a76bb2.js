3bc89113d6e314c2159fdb184e1577316507f739
itsmushi <jmushi175@gmail.com>
2022-04-07 11:20:54 +0300

    updated site

---
+++ precache-manifest.6d6cca173f0007fc4aea4c2215a76bb2.js
@@ -0,0 +1,206 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "983e971f45d526884047e4d461c74f3d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0d4c394dcdd2a27dbaa2",
+    "url": "./static/css/main.04ce090d.chunk.css"
+  },
+  {
+    "revision": "1851ac4debf065d216f7",
+    "url": "./static/js/2.2963cbeb.chunk.js"
+  },
+  {
+    "revision": "4cf5c6c6f464cc15458a86cbdc669a15",
+    "url": "./static/js/2.2963cbeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d4c394dcdd2a27dbaa2",
+    "url": "./static/js/main.9c98b1bf.chunk.js"
+  },
+  {
+    "revision": "85c64dbc66e06d7d49eb",
+    "url": "./static/js/runtime-main.207de92b.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "./static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "./static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "90773b6158663ab0fe78b32680733677",
+    "url": "./static/media/GoogleSans-BoldItalic.90773b61.ttf"
+  },
+  {
+    "revision": "0ecddcdeccb7761ce899aa9ad9f0ac3f",
+    "url": "./static/media/GoogleSans-Italic.0ecddcde.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "./static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "8fd3737925e83c87d78a13700ccf9a62",
+    "url": "./static/media/GoogleSans-MediumItalic.8fd37379.ttf"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "./static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "f2c9046eb458b1a9a0e2b4bc5210cd1a",
+    "url": "./static/media/HISP-LOGO.f2c9046e.svg"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "./static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "a1051a228140c97862ba9a7f321c92b6",
+    "url": "./static/media/buld.a1051a22.jpg"
+  },
+  {
+    "revision": "404ae7bc061a03e399daa61344c78566",
+    "url": "./static/media/cwoc.404ae7bc.png"
+  },
+  {
+    "revision": "82473004d117a65c8ed30396c6299763",
+    "url": "./static/media/efi.82473004.png"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "./static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "./static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "a9c4bb7348f42626454c988dbde1d0a0",
+    "url": "./static/media/fa-brands-400.a9c4bb73.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "./static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "./static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "./static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "7b9568e6389b1f8ae0902cd39665fc1e",
+    "url": "./static/media/fa-regular-400.7b9568e6.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "./static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "./static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "./static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "./static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "./static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "./static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "./static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "c2801fb415f03c7b170934769d7b5397",
+    "url": "./static/media/fa-solid-900.c2801fb4.svg"
+  },
+  {
+    "revision": "54f6721948491acb1997fd43496cd7df",
+    "url": "./static/media/flutter.54f67219.png"
+  },
+  {
+    "revision": "6364dbb3a5b59c28566eac7e6aaacd04",
+    "url": "./static/media/gdg.6364dbb3.png"
+  },
+  {
+    "revision": "0ba2aa20e2c2ce80e9a2db5b07198464",
+    "url": "./static/media/github.0ba2aa20.png"
+  },
+  {
+    "revision": "0c8a4c114ad00f7854e91550379b9b91",
+    "url": "./static/media/googleAssistant.0c8a4c11.jpg"
+  },
+  {
+    "revision": "7a2dbf7a2595600c00118eea8a7a52f8",
+    "url": "./static/media/hackathon1.7a2dbf7a.svg"
+  },
+  {
+    "revision": "36d7942a201d6e2b83b55876e8433ccf",
+    "url": "./static/media/iit.36d7942a.png"
+  },
+  {
+    "revision": "ccf886614a9c68f209d3e95c2626dd99",
+    "url": "./static/media/ino.ccf88661.png"
+  },
+  {
+    "revision": "847873a85df8045d1d51cc6cf9cc97d7",
+    "url": "./static/media/localguide.847873a8.gif"
+  },
+  {
+    "revision": "9225d7673546a92fb8b211102f993a71",
+    "url": "./static/media/logo_ud.9225d767.png"
+  },
+  {
+    "revision": "1af43aacbe90cdb370774575ec858cab",
+    "url": "./static/media/logo_ud2.1af43aac.png"
+  },
+  {
+    "revision": "bc5cfd3a887013976863247e66c389a2",
+    "url": "./static/media/mlh-logo.bc5cfd3a.svg"
+  },
+  {
+    "revision": "53dd27a8801e3a4216b9c9e217dfc7d0",
+    "url": "./static/media/mlh.53dd27a8.png"
+  },
+  {
+    "revision": "c9fe72874bc3cc63fa1f8c073307001b",
+    "url": "./static/media/mushi_prof.c9fe7287.JPG"
+  },
+  {
+    "revision": "22f11067f2d745aaceb51e485720635d",
+    "url": "./static/media/postman.22f11067.jpg"
+  },
+  {
+    "revision": "c108f55777ceaa6690606a207b737d0a",
+    "url": "./static/media/postman.c108f557.png"
+  },
+  {
+    "revision": "4179bf09f64edaaca5a3886aab441646",
+    "url": "./static/media/uiss.4179bf09.png"
+  },
+  {
+    "revision": "246ad3ede6a55b0102afa9871cfcbf21",
+    "url": "./static/media/valora.246ad3ed.jpg"
+  },
+  {
+    "revision": "02fdae4350ca45cabdc9c40cc11efc2a",
+    "url": "./static/media/wrighter.02fdae43.jpg"
+  }
+]);
